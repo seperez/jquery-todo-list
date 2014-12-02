28cb83d90eee89930f1342be904f6c8725f0b393
Sebastin M. Perez <perezsebastianm@gmail.com>
2014-12-02 01:45:26 -0300

    Create add action

--- app/scripts/todo.js
+++ app/scripts/todo.js
@@ -7,11 +7,41 @@ var config = {
 };
 
 var Todo = function(options){
-    console.log('new todo');
+    this.options = $.extend(options, {
+        todoAppEl: '.todo-app',
+        inputEl: '.todo-input',  
+        listEl: '.todo-list'
+    });
+
+    this.$app = $(this.options.todoAppEl);
+    this.$input = $(this.options.inputEl);
+    this.$list = $(this.options.listEl);
+    this.options.items = [];
+
+    this.bindEvents = function(){
+        this.$app.on('keypress', this.options.inputEl, this, this.add);
+    };
+
+    this.bindEvents();
 };
 
 Todo.prototype.add = function(event){
-    console.log('add');
+    var that = event.data;
+
+    if (event.which === config.keys.enter) {
+
+        var todoItem = 
+                '<li>' +
+                    '<div class="view">' +
+                        '<input class="toggle" type="checkbox">' +
+                        '<label data="">' + ' ' + that.$input.val() + '</label>' +
+                        '<a class="destroy"></a>' +
+                    '</div>' +
+                '</li>';
+
+        that.$list.append(todoItem);
+        that.$input.val('');
+    }
 };
 
 Todo.prototype.remove = function(){
