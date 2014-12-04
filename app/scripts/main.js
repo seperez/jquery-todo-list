b3aa58decfa0e81f0034f94787b503e6b82700b3
Sebastin M. Perez <perezsebastianm@gmail.com>
2014-12-03 21:34:13 -0300

    Create toggle done action

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(function(){
-    var myTodoList = new Todo();
+    new Todo();
 });
 
 
--- app/scripts/todo.js
+++ app/scripts/todo.js
@@ -11,17 +11,19 @@ var Todo = function(options){
         todoAppEl: '.todo-app',
         inputEl: '.todo-input',  
         listEl: '.todo-list',
-        removeEl: '.todo-item-remove'
+        removeEl: '.todo-item-remove',
+        doneEl: '.todo-item-toggle-done'
     });
 
     this.$app = $(this.options.todoAppEl);
     this.$input = $(this.options.inputEl);
     this.$list = $(this.options.listEl);
-    this.options.items = [];
+    this.items = [];
 
     this.bindEvents = function(){
         this.$app.on('keypress', this.options.inputEl, this, this.add);
         this.$app.on('click', this.options.removeEl, this, this.remove);
+        this.$app.on('click', this.options.doneEl, this, this.toggleDone);
     };
 
     this.bindEvents();
@@ -33,13 +35,13 @@ Todo.prototype.add = function(event){
     if (event.which === config.keys.enter) {
 
         var todoItem = 
-                '<li>' +
-                    '<div class="todo-item">' +
-                        '<input class="todo-item-toggle-done" type="checkbox">' +
-                        '<label>' + that.$input.val() + '</label>' +
-                        '<a href="#" class="todo-item-remove"></a>' +
-                    '</div>' +
-                '</li>';
+            '<li>' +
+                '<div class="todo-item">' +
+                    '<input class="todo-item-toggle-done" type="checkbox">' +
+                    '<label>' + that.$input.val() + '</label>' +
+                    '<a href="#" class="todo-item-remove"></a>' +
+                '</div>' +
+            '</li>';
 
         that.$list.append(todoItem);
         that.$input.val('');
@@ -51,8 +53,14 @@ Todo.prototype.remove = function(event){
     $(this).closest('li').remove();
 };
 
-Todo.prototype.done = function(){
-    console.log('done');
+Todo.prototype.toggleDone = function(event){
+    var that = event.data;
+
+    if($(this).is(':checked')){
+        $(this).closest('li').appendTo(that.$list);
+    }else{
+        $(this).closest('li').prependTo(that.$list);
+    }
 };
 
 Todo.prototype.clear = function(){
